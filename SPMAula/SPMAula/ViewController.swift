b11038e892c72d159bc9a9e102c0af880d06f66d
Juliana Vigato Pavan <ju.vigato3@gmail.com>
2020-04-07 16:55:14 -0300

    inicializando o projeto

---
+++ SPMAula/SPMAula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMAula
+//
+//  Created by Juliana Vigato Pavan on 07/04/20.
+//  Copyright © 2020 Juliana Vigato Pavan. All rights reserved.
+//
+
+import UIKit
+import reflectium_ipsum
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Sua reflexão é '\(ReflectionGenerator.generatiumReflectium())'")
+    }
+}
+
